0d5bfe9e438291ea5070429592a6065209271cf1
iosif-tihonenkov <iosif.tihonenkov@gmail.com>
2025-04-11 23:44:03 +0300

    Доработка

--- bot.py
+++ bot.py
@@ -1,17 +1,13 @@
-from telegram.ext import Application, CommandHandler
-from config import BOT_TOKEN
 from handlers.user import start_user
 from handlers.admin import start_admin
+from config import ADMIN_CHAT_ID
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.effective_chat.id == ADMIN_CHAT_ID:
+        return await start_admin(update, context)
+    return await start_user(update, context)
 
 def main():
     app = Application.builder().token(BOT_TOKEN).build()
-
-    # Регистрируем команды
-    app.add_handler(CommandHandler("start", start_user))  # Для пользователей
-    app.add_handler(CommandHandler("start", start_admin))  # Для админов (проверка внутри функции)
-
+    app.add_handler(CommandHandler("start", start))  # Только один обработчик
     app.run_polling()
-
-if __name__ == "__main__":
-    main()
