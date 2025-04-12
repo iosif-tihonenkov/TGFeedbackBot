c801228685e6318c2b8eaab65988c5caf3aaf71f
iosif-tihonenkov <iosif.tihonenkov@gmail.com>
2025-04-12 17:21:07 +0300

    Ага

--- handlers/admin.py
+++
@@ -1,7 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-from config import ADMIN_CHAT_ID
-
-async def start_admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.effective_chat.id == ADMIN_CHAT_ID:  # Проверяем, что это админский чат
-        await update.message.reply_text("Добрый день, администратор")
